9f058e1ffcdc9344aacff308b01d1b3dfce11483
magomed_n <naurbaev2020@gmail.com>
2023-12-14 00:32:28 +0300

    Обработка основных событий приложения

--- js/main.js
+++ js/main.js
@@ -37,6 +37,147 @@ const App = (function (ItemCtrl, storageCtrl, UICtrl) {
       .addEventListener("click", goalAddSubmit);
   };
 
+  const itemAddSubmit = (e) => {
+    // Получить входные данные формы из контроллера UI
+    const input = UICtrl.getItemInput();
+
+    // Проверить наличие ввода имени и калорий
+    if (input.name !== "" && input.calories !== "") {
+      // Добавить элемент
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+      // Добавить элемент в список UI
+      UICtrl.addListItem(newItem);
+
+      // Получить общее количество калорий
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Отобразить общее количество калорий в UI
+      UICtrl.showTotalCalories(totalCalories);
+      UICtrl.showProgress(undefined, totalCalories);
+
+      // Сохранить в localStorage
+      storageCtrl.storeItem(newItem);
+
+      // Очистить поля
+      UICtrl.clearInput();
+    }
+
+    e.preventDefault();
+  };
+
+  const goalAddSubmit = (e) => {
+    // Получить входные данные формы из контроллера UI
+    const goalInput = UICtrl.getGoalInput();
+
+    if (goalInput) {
+      UICtrl.showDailyGoal(goalInput);
+
+      // Сохранить в localStorage
+      storageCtrl.storeGoal(goalInput);
+    }
+
+    UICtrl.showProgress(goalInput);
+    const dialog = document.getElementById("daily-goal");
+    dialog.close();
+    e.preventDefault();
+  };
+
+  // нажатие на кнопку редактирования элемента
+  const itemEditClick = (e) => {
+    if (e.target.classList.contains("edit-item")) {
+      // Получить идентификатор элемента списка (item-0, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+
+      // Разбить на массив
+      const listIdArr = listId.split("-");
+
+      // Получить фактический идентификатор
+      const id = parseInt(listIdArr[1]);
+
+      // Получить элемент
+      const itemToEdit = ItemCtrl.getItemById(id);
+
+      // Установить текущий элемент
+      ItemCtrl.setCurrentItem(itemToEdit);
+
+      // Добавить элемент в форму
+      UICtrl.addItemToForm();
+    }
+
+    e.preventDefault();
+  };
+
+  // Обновить отправку элемента
+  const itemUpdateSubmit = (e) => {
+    // Получить ввод элемента
+    const input = UICtrl.getItemInput();
+
+    // Обновить элемент
+    const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
+
+    // Обновить пользовательский интерфейс
+    UICtrl.updateListItem(updatedItem);
+
+    // Получить общее количество калорий
+    const totalCalories = ItemCtrl.getTotalCalories();
+    // Отобразить общее количество калорий в UI
+    UICtrl.showTotalCalories(totalCalories);
+    UICtrl.showProgress(undefined, totalCalories);
+
+    // Обновить хранилище локальных данных
+    storageCtrl.updateItemStorage(updatedItem);
+
+    UICtrl.clearEditState();
+
+    e.preventDefault();
+  };
+
+  // Событие кнопки удаления
+  const itemDeleteSubmit = (e) => {
+    // Получить текущий элемент
+    const currentItem = ItemCtrl.getCurrentItem();
+
+    // Удалить из структуры данных
+    ItemCtrl.deleteItem(currentItem.id);
+
+    // Удалить из UI
+    UICtrl.deleteListItem(currentItem.id);
+
+    storageCtrl.deleteItemFromStorage(currentItem.id);
+    // Получить общее количество калорий
+    const totalCalories = ItemCtrl.getTotalCalories();
+    // Отобразить общее количество калорий в UI
+    UICtrl.showTotalCalories(totalCalories);
+    UICtrl.showProgress(undefined, totalCalories);
+    // Удалить из хранилища локальных данных
+
+    UICtrl.clearEditState();
+
+    e.preventDefault();
+  };
+
+  // Событие очистки всех элементов
+  const clearAllItemsClick = () => {
+    // Удалить все элементы из структуры данных
+    ItemCtrl.clearAllItems();
+
+    // Получить общее количество калорий
+    const totalCalories = ItemCtrl.getTotalCalories();
+    // Отобразить общее количество калорий в UI
+    UICtrl.showTotalCalories(totalCalories);
+
+    // Удалить из UI
+    UICtrl.removeItems();
+
+    UICtrl.showProgress(undefined, totalCalories);
+
+    // Очистить из хранилища локальных данных
+    storageCtrl.clearItemsFromStorage();
+
+    // Скрыть UL
+    UICtrl.hideList();
+  };
+
   // Общие методы App
   return {
     init: () => {
